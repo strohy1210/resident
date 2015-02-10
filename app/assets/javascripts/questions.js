37fd6593483f49e36b059f14de142e8180c029c0
strohy120 <sarah.stroh@flatironschool.com>
2015-02-10 13:21:15 -0500

    minor

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,6 +1,11 @@
 $(function(){
   
   $('#demoModal').modal('show');
+
+  $('input[name=ans]').change(function(){
+     $('form').submit();
+   });
+  
   $('.answers').on('click', ".answer",function(){
 
     $(this).find('input').get(0).checked=true;
